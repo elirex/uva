b945bbefa29a34ab93fcb86bdad4eff29f0a2963
Elirex <sheng79225@gmail.com>
2015-11-23 23:26:24 +0800

    UVa10107 - What is the Median?

--- problems/UVa10107_WhatIsTheMedian.cpp
+++ problems/UVa10107_WhatIsTheMedian.cpp
@@ -1,33 +1,30 @@
 /*
- * UVa 10107 What is the Median?
+ * UVa 10107 - What is the Median?
  * Author: Elirex (Wang Sheng-Yuan)
- * Date: 2015-10-20
+ * Date: 2015-11-23
  */
-#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+vector<unsigned int> sequence;
+
 int main(int argc, const char *argv[]) {
-	int n, index;
-	int array[10001];
-	int size = 0;
-	while(cin >> n) {
-		for(index = 0; index < size; ++index) {
-			if(n > array[index]) {
-				for(int j = size; j >= index; --j) {
-					array[j] = array[j-1];
-				}
-				break;
-			}
-		}
-		array[index] = n;
-		size++;
+    
+    unsigned int n;
+    vector<unsigned int>::size_type length;
+    while(scanf("%d", &n) != EOF) {
+        sequence.push_back(n);
+        sort(sequence.begin(), sequence.end());
+        length = sequence.size();
+        n = length / 2; 
+        if(length % 2 == 0) {
+            printf("%d\n", (sequence[n] + sequence[n-1]) / 2);
+        } else {
+            printf("%d\n", sequence[n]);
+        }
+    }
 
-		if(size % 2) {
-			cout << array[size/2] << endl;
-		} else {
-			cout << (array[size/2-1] + array[size/2]) / 2 << endl;
-		}
-	}
-
-	return 0;
+    return 0;
 }
